2814d528af5a0290d54ba1b3cedd92ac6f4cd836
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-10 16:06:37 -0800

    pass second integration test for updating name

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,9 +8,19 @@ describe('add new stylist', {:type => :feature}) do
     visit('/')
     fill_in('stylist_name', :with => 'Wacky Wanda')
     click_button('Add Stylist')
-    expect(page).to have_content('Success!')
     click_link('Return to Homepage')
     click_link('Wacky Wanda')
     expect(page).to have_content('Information for Wacky Wanda')
+  end
+end
+
+describe('change stylist name', {:type => :feature}) do
+  it('allows user to change stylist name') do
+    stylist = Stylist.new({:name => 'Wacky Wanda', :id => nil})
+    stylist.save()
+    visit("/stylist/#{stylist.id()}")
+    fill_in("new_name", :with => 'Moby Dick')
+    click_button('Update Name')
+    expect(page).to have_content('Moby Dick')
   end
 end
